76ed0fbb51c11b7077bd312801c4f76599c7944c
flo <flo33.lav@gmail.com>
2019-01-02 21:57:28 +0100

    child activity

--- app/src/main/java/com/wcs/parent_activity_intent/MyTabActivity.java
+++ app/src/main/java/com/wcs/parent_activity_intent/MyTabActivity.java
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.elyot.parent_activity_intent.R;
 
